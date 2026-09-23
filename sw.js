2334e4e7b77d042d117c3784687c3a30935ce0d7
mohdaie <ayiepts@gmail.com>
2026-09-23 16:09:43 +0800

    Add offline app shell

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'meeting-pocket-shell-v0.1.0';
+const SHELL = ['./','./index.html','./styles.css','./app.js','./db.js','./ai.js','./manifest.webmanifest','./icons/icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(req).then(hit => hit || fetch(req).then(res => {
+    const clone = res.clone();
+    caches.open(CACHE).then(c => c.put(req, clone));
+    return res;
+  })));
+});
