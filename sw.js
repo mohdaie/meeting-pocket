1fd666b349a10e54a26b565d9ba0f7bdbed50ff7
mohdaie <ayiepts@gmail.com>
2026-09-23 16:56:25 +0800

    Bump Meeting Pocket cache to v0.3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meeting-pocket-shell-v0.2.2';
+const CACHE = 'meeting-pocket-shell-v0.3.0';
 const SHELL = ['./','./index.html','./styles.css','./app.js','./db.js','./ai.js','./manifest.webmanifest','./icons/icon.svg'];
 
 self.addEventListener('install', event => {
